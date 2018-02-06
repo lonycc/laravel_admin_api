6f0e51793145afdadb52d689d3e1c051d1138301
tony <xcc2533919@163.com>
2018-02-06 17:05:46 +0800

    Add files via upload

---
+++ database/migrations/2018_02_06_144642_create_lotto_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLottoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lotto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('info', 50)->nullable()->default('');
+        });
+        Schema::create('lottery_lotto_data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('main', 50);
+            $table->string('other', 200)->nullable()->default('');
+            $table->integer('lotto_id');
+        });
+        Schema::create('lottery_lotto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('lottery_id');
+            $table->integer('lotto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lotto');
+    }
+}
