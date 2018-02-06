4f2ceedc53962f10ea3cc286d576d742a073c042
tony <xcc2533919@163.com>
2018-02-06 17:10:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,14 @@ Route::group(['namespace' => 'Admin', 'middleware' => 'permissionCheck'], functi
     Route::resource('users', 'UserController', ['except' => 'show']);
     Route::resource('roles', 'RoleController', ['except' => 'show']);
     Route::resource('permissions', 'PermissionController', ['except' => 'show']);
+    Route::resource('lottery', 'LotteryController', ['except' => 'show']);
+    Route::resource('lottodata', 'LottoDataController', ['except' => 'show']);
     Route::get('/roles/{role}/permission', 'RoleController@permission');
     Route::post('/roles/{role}/permission', 'RoleController@storePermission');
     Route::get('/users/{user}/role', 'UserController@role');
     Route::post('/users/{user}/role', 'UserController@storeRole');
+    Route::get('/lottery/{lottery}/award', 'LotteryController@award');
+    Route::post('/lottery/{lottery}/award', 'LotteryController@storeAward');
+    Route::get('/lottery/{lottery}/lotto', 'LotteryController@lotto');
+    Route::post('/lottery/{lottery}/lotto', 'LotteryController@storeLotto');
 });
